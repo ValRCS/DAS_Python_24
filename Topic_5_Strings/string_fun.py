ba6be731e3e4702bb4746342dbd6982b8110317c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-05-09 19:10:52 +0300

    added start of string fun

---
+++ Topic_5_Strings/string_fun.py
@@ -0,0 +1,15 @@
+# let's explore strings in Python
+
+# Strings are sequences of characters
+# Strings are immutable
+# Strings can be indexed and sliced
+
+# Strings can be created using single quotes
+food = 'pizza'
+print(food)
+
+# Strings can be created using double quotes
+drink = "coffee"
+print(f"I like to drink {drink}")
+
+# in Python there is no difference between single and double quotes
