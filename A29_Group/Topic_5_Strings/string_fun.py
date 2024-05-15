4805744de3d5c9d15975f1089fb1f0eb4c7035db
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-05-15 18:41:30 +0300

    added string fun stub

---
+++ A29_Group/Topic_5_Strings/string_fun.py
@@ -0,0 +1,10 @@
+# let's talk about strings in Python
+
+# Strings are a sequence of characters 
+# Python actually does not have a character data type, a single character is a string of length 1
+# Strings are immutable, meaning you cannot change them in place
+# Strings can be indexed and sliced
+
+# Strings can be created with single, double or triple quotes
+food = 'kartupelis'
+print(food)
