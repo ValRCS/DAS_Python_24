07daf2343ef66978582f122b018954f97e92f1eb
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-05-23 18:19:23 +0300

    tuple stub

---
+++ Topic_9_Tuples_and_Sets/tuples.py
@@ -0,0 +1,15 @@
+# Python Tuples (a, b, c, d, e) - Latviski - korteži, nemaināmi saraksti
+# some properties of tuples
+# Immutable - we don't change the values of a tuple itself
+# Usage - pass data around, return multiple values from a function
+# Tuples are faster than lists
+# Tuples are used as dictionary keys - tuples are generally hashable
+# Tuples uses for packing and unpacking
+# Tuples are indexed, ordered, and iterable
+# Tuples have their own methods, but fewer than lists
+# Tuples can be nested just like lists
+# Philosophically - Tuples are for heterogeneous data (different data types)
+#  lists are best for homogeneous data - same type of data
+# Python lets us use either for either, but it's a good practice to use them as intended
+# Documentation: https://docs.python.org/3/library/stdtypes.html#tuple
+
