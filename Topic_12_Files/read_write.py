f488be69f129bc9056b91efe8011d39fabd11516
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-05-29 20:34:22 +0300

    frost and read write stub

---
+++ Topic_12_Files/read_write.py
@@ -0,0 +1,13 @@
+# Reading and Writing Files
+# Docs: https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+# Real Python: https://realpython.com/read-write-files-python/
+
+# What is a File?
+# A file is a named location on disk to store related information. 
+# It is used to permanently store data in a non-volatile memory (e.g. hard disk).
+# Idea of persistent storage that outlives the execution of a program.
+# files have multiple formats: text, binary, etc.
+# text files we define the encoding (e.g. UTF-8, ASCII, etc.)
+# binary files are not human readable
+# text files can have multiple formats: TXT, CSV, JSON, XML, HTML, PY, etc.
+# typically these days we deal with Unicode text files encoded in UTF-8
