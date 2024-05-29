c3a211959bfc558383971d54633e35d47fc5d818
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-05-29 19:19:52 +0300

    Sets stub

---
+++ A29_Group/Topic_9_Tuples_Sets/sets.py
@@ -0,0 +1,14 @@
+# Sets - Kopas Latviski
+# Sets are unordered collections of unique elements. 
+# We can construct them by using the set() function.
+# Set properties:
+# Sets are unordered - ! no indexing !
+# Sets do not allow duplicates - all items are unique
+# Sets are mutable - we can add and remove items
+
+# Usage of sets
+# - remove duplicates from a lists, tuples, etc
+# - check for membership - quicker than lists or tuples for large collections
+# - set operations - union, intersection, difference, etc
+
+# Documentation: https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset
